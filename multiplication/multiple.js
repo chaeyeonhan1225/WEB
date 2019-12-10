2aa1e168cbfdecff309128782510ce6fffbb0527
chaeyeonhan1225 <gkscodus11@gmail.com>
2019-12-10 23:21:21 +0900

    구구단css,js 수정함

--- multiplication/multiple.js
+++ multiplication/multiple.js
@@ -1,9 +1,9 @@
 function multiple(n){
+    document.write("<div>");
     for(let i=1;i<=9;++i){
-        document.write(`${n} x ${i} = ${n*i} `);
-        if(i%3==0)
-            document.write("<br>");
+        document.write(`${n} x ${i} = ${n*i}<br>`);
     }
+    document.write("</div>");
 }
 
 for(let i=2;i<=9;++i){
